77bbe13bdc01f159b3bf3b75be2d673790ed97bb
Newton Galiza <newtonjgaliza@gmail.com>
2017-02-16 16:35:06 -0300

    Add files via upload

---
+++ Blink_pin/blink_pin.ino
@@ -0,0 +1,19 @@
+int pin = 9;
+int ligado = 500;
+int desligado = 300;
+
+
+void setup() {
+  
+  pinMode(pin, OUTPUT);
+  
+}
+
+void loop() {
+ 
+  digitalWrite(pin, HIGH);
+  delay(ligado);
+  digitalWrite(pin, LOW);
+  delay(desligado);
+
+}
